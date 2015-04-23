4b34133bbd710cf0944ad80af979ba0b0c6c5303
HsuTing <hsuting0106@gmail.com>
2015-04-23 09:40:28 +0800

    temp

--- javascript.js
+++ javascript.js
@@ -27,10 +27,22 @@ d3.json("data.json", function(root) {
 	    .selectAll(".node")
 	    .data(treemap.nodes)
 	  .enter().append("div")
+		.attr("class", "node")
+	    .call(position)
 	    .style("background", function(d) { return d.children ? color(d.name) : null; })
 	    .text(function(d) { return d.children ? null : d.name; });
 
+	node
+	    .data(treemap.value(function(d) { return d.year; }).nodes(root))
+	  .transition()
+	    .duration(1500)
+	    .call(position);
+
 	function position() {
 		this
+		  .style("left", function(d) { return d.x + "px"; })
+		  .style("top", function(d) { return d.y + "px"; })
+		  .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+		  .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
 	}
 });
